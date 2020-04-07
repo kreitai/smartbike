341a480eb25d85cda6848943d83c8434675aab53
Alex Kuzmin <alex.kuzmin@protonmail.ch>
2020-04-07 12:10:28 +0800

    Add location permission request
    Moving the camera to user's current location. Updated dependencies.

--- app/src/main/java/com/kreitai/smartbike/map/view/StationMapFragment.kt
+++ app/src/main/java/com/kreitai/smartbike/map/view/StationMapFragment.kt
@@ -24,15 +24,24 @@
 
 package com.kreitai.smartbike.map.view
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
+import com.kreitai.smartbike.core.utils.PermissionUtils
 import com.kreitai.smartbike.core.view.BaseFragment
 import com.kreitai.smartbike.databinding.FragmentStationMapBinding
 import com.kreitai.smartbike.map.ext.toBitmapDescriptor
@@ -48,10 +57,13 @@ class StationMapFragment :
     OnMapReadyCallback {
 
     companion object {
-        private const val INITIAL_ZOOM = 13.0f
+        private const val INITIAL_ZOOM = 17.0f
         private const val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 999
     }
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var permissionDenied: Boolean = false
     private var googleMap: GoogleMap? = null
 
     override val toolbarResId: Int
@@ -72,6 +84,14 @@ class StationMapFragment :
     }
 
     private lateinit var mapView: MapView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        fusedLocationClient = LocationServices
+            .getFusedLocationProviderClient(requireActivity())
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -117,9 +137,45 @@ class StationMapFragment :
 
     override fun onMapReady(googleMap: GoogleMap?) {
         this.googleMap = googleMap
-        val taipei = LatLng(25.033, 121.52)
-        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(taipei, INITIAL_ZOOM))
+        enableMyLocation()
         getViewModel().fetchStations()
+    }
+
+    private fun enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            googleMap?.isMyLocationEnabled = true
+            val locationResult: Task<Location?> = fusedLocationClient.lastLocation
+            locationResult.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Set the map's camera position to the current location of the device.
+                    val location = task.result
+                    val currentLatLng = LatLng(
+                        location?.latitude ?: 25.033,
+                        location?.longitude ?: 121.52
+                    )
+                    googleMap?.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            currentLatLng,
+                            INITIAL_ZOOM
+                        )
+                    )
+                }
+            }
+
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            PermissionUtils.requestPermission(
+                requireActivity() as AppCompatActivity?, LOCATION_PERMISSION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION, true
+            )
+            val taipei = LatLng(25.033, 121.52)
+            googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(taipei, INITIAL_ZOOM))
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -137,6 +193,19 @@ class StationMapFragment :
     override fun onResume() {
         super.onResume()
         mapView.onResume()
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError()
+            permissionDenied = false
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private fun showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+            .newInstance(true).show(parentFragmentManager, "dialog")
     }
 
     override fun onStart() {
@@ -162,6 +231,29 @@ class StationMapFragment :
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+        if (PermissionUtils.isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation()
+        } else {
+            // Permission was denied. Display an error message
+            // Display the missing permission error dialog when the fragments resume.
+            permissionDenied = true
+        }
     }
 
 }
